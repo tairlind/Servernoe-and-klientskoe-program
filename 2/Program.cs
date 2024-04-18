afbb08e683ab3c7bd1e93c6c803226e67181bfbf
n <hhhrrr@gmail.com>
2024-04-18 12:34:06 +0300

    Добавьте файлы проекта.

---
+++ 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 8888);
+
+                Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    sender.Connect(remoteEP);
+
+                    Console.WriteLine("Connected to {0}", sender.RemoteEndPoint.ToString());
+
+                    byte[] msg = Encoding.ASCII.GetBytes("Привет, сервер!");
+                    int bytesSent = sender.Send(msg);
+
+                    byte[] bytes = new byte[1024];
+                    int bytesRec = sender.Receive(bytes);
+                    Console.WriteLine("Полученный ответ: {0}", Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: {0}", e.Message);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ Servernoe and klientskoe program/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.GetEncoding(866);
+            Console.WriteLine("Однопоточный сервер запущен");
+
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 8888);
+
+            Socket sock = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                sock.Bind(ipEndPoint);
+                sock.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Слушаем, порт {0}", ipEndPoint);
+
+                    Socket s = sock.Accept();
+                    string data = null;
+                    byte[] bytes = new byte[1024];
+                    int bytesRec = s.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                    Console.WriteLine("Полученный текст: " + data);
+
+                    byte[] msg = Encoding.ASCII.GetBytes("Сервер получил ваше сообщение.");
+                    s.Send(msg);
+
+                    s.Shutdown(SocketShutdown.Both);
+                    s.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+
+            Console.WriteLine("\nНажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
